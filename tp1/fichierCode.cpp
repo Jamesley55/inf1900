d38ff2569284816af53fdb47292afafcfdeb1a1b
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-09-06 21:25:33 -0400

    TP1 Done

--- tp1/fichierCode.cpp
+++ tp1/fichierCode.cpp
@@ -1,53 +1,60 @@
-#include <avr/io.h> 
+#include <avr/io.h>
 #define F_CPU 8000000
 #include <util/delay.h>
 
-void Couleur(){
-  DDRB = 0xff; // PORT B est en mode sortie
-  
-  for (;;){
-    PORTB = (1 << 0); // rouge
+void Couleur()
+{
+    DDRB = 0xff; // PORT B est en mode sortie
 
-    _delay_ms(1000); //1 sec
+    for (;;)
+    {
+        PORTB = (1 << 0); // rouge
 
-    PORTB = (1 << 1); // vert
+        _delay_ms(1000); //1 sec
 
-    _delay_ms(1000); // 1 sec
+        PORTB = (1 << 1); // vert
 
-    double compteur = 0;
-    while (compteur <= 100){  // Ambre 
-       PORTB = 0x02;
-      
-      _delay_ms(10); 
+        _delay_ms(1000); // 1 sec
 
-      PORTB = 0x01;
+        double compteur = 0;
+        while (compteur <= 100)
+        { // Ambre
+            PORTB = 0x02;
 
-      _delay_ms(2); 
-      compteur++;
+            _delay_ms(10);
+
+            PORTB = 0x01;
+
+            _delay_ms(2);
+            compteur++;
+        }
     }
-    
-  }
 }
 
-void InterupteurAmbre(){
-  DDRB = 0xff;  // PORT B est en mode sortie
-  DDRB = 0x0;  // PORT A est en mode entree 
-  for(;;){
-    if(PIND & 0x04){
-      _delay_ms(10);
-        while (PIND & 0x04){  
+void InterupteurAmbre()
+{
+    DDRB = 0xff; // PORT B est en mode sortie
+    DDRB = 0x0;  // PORT A est en mode entree
+    for (;;)
+    {
+        if (PIND & 0x04)
+        {
+            _delay_ms(10);
+            while (PIND & 0x04)
+            {
 
-          PORTB = 0x01;
-          _delay_ms(2);    
+                PORTB = 0x01;
+                _delay_ms(2);
+            }
         }
-      }
     }
     PORTB = 0x00;
-  }
+}
 }
 
-int main(){
+int main()
+{
     Couleur();
     InterupteurAmbre();
-  return 0;
+    return 0;
 }
